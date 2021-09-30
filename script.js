44d35db00bbbe66ed1189ce215b57a09c4b03074
Onkar Madli <onkarmadli@gmail.com>
2021-09-30 17:49:24 +0530

    Update styles

--- script.js
+++ script.js
@@ -3,21 +3,27 @@ const stocksQuantity = document.querySelector("#stocks-quantity");
 const currPrice = document.querySelector("#current-price");
 const submitBtn = document.querySelector("#submit-btn");
 const outputBox = document.querySelector("#output-box");
-
+const body = document.querySelector("body")
 
 function showOutput(message){
     outputBox.innerText = message;
 }
 
 function calculateProfitLoss(initial, quantity, current){
+    console.log(initial);
+    console.log(current);
     if(initial > current){
         const loss = initial-current;
         const lossPercent = ((loss/initial)*100).toFixed(2);
-        showOutput("The loss is ${loss} and its percent is ${lossPercent}");
+        showOutput(`The loss is ${loss} and its percent is ${lossPercent}%`);
+        body.style.transition = "background-color 4s ease-out";
+        body.style.backgroundColor = "red";
     } else if(initial < current){
         const profit = current-initial;
         const profitPercent = ((profit/initial)*100).toFixed(2);
-        showOutput("The loss is ${profit} and its percent is ${profitPercent}");
+        showOutput(`The profit is ${profit} and its percent is ${profitPercent}%`);
+        body.style.transition = "background-color 4s ease-out";
+        body.style.backgroundColor = "green";
     } else {
 
     }
