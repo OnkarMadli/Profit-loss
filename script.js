2696428fa5cdc2d5408c2d5695ae52cc892e489d
Onkar Madli <onkarmadli@gmail.com>
2021-10-01 12:00:45 +0530

    Add case for negative inputs

--- script.js
+++ script.js
@@ -10,22 +10,28 @@ function showOutput(message){
 }
 
 function calculateProfitLoss(initial, quantity, current){
-    if(initial > current){
-        const loss = initial-current;
-        const lossPercent = ((loss/initial)*100).toFixed(2);
-        showOutput(`The loss is ${loss} and its percent is ${lossPercent}%`);
-        body.style.transition = "background-color 4s ease-out";
-        body.style.backgroundColor = "red";
-    } else if(initial < current){
-        const profit = current-initial;
-        const profitPercent = ((profit/initial)*100).toFixed(2);
-        showOutput(`The profit is ${profit} and its percent is ${profitPercent}%`);
-        body.style.transition = "background-color 4s ease-out";
-        body.style.backgroundColor = "green";
-    } else {
-        showOutput(`You neither lost nor gained anything`);
+    if(initial<1 || current<1){
+        showOutput(`Initial Price/Current price cannot be 0 or in negatives`);
         body.style.transition = "background-color 4s ease-out";
         body.style.backgroundColor = "rgb(235, 221, 26)";
+    } else {
+        if(initial > current){
+            const loss = initial-current;
+            const lossPercent = ((loss/initial)*100).toFixed(2);
+            showOutput(`The loss is ${loss} and its percent is ${lossPercent}%`);
+            body.style.transition = "background-color 4s ease-out";
+            body.style.backgroundColor = "red";
+        } else if(initial < current){
+            const profit = current-initial;
+            const profitPercent = ((profit/initial)*100).toFixed(2);
+            showOutput(`The profit is ${profit} and its percent is ${profitPercent}%`);
+            body.style.transition = "background-color 4s ease-out";
+            body.style.backgroundColor = "green";
+        } else {
+            showOutput(`You neither lost nor gained anything`);
+            body.style.transition = "background-color 4s ease-out";
+            body.style.backgroundColor = "rgb(235, 221, 26)";
+        }
     }
 }
 
